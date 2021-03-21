6d4420bdb7f32fbffd609ca1bc1a611d1b3e72a6
SirCouqounFace <61544417+SirCouqounFace@users.noreply.github.com>
2021-03-21 14:23:35 +0100

    Login ablak alapjainak hozzáadása
    Login ablak alapjainak hozzáadása, valamint controller csontvázának létrehozása

---
+++ src/main/java/magus/controllers/LoginController.java
@@ -0,0 +1,33 @@
+package magus.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private TextField tb_Username;
+
+    @FXML
+    private TextField tb_Password;
+
+    @FXML
+    private Button bt_Login;
+
+    @FXML
+    private Button bt_Register;
+
+    @FXML
+    void bt_LoginPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void bt_RegisterPressed(ActionEvent event) {
+
+    }
+
+}
+
