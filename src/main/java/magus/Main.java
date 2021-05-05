c5b2908b66d60e819531a13803ee736fbf57b6a1
SirCouqounFace <61544417+SirCouqounFace@users.noreply.github.com>
2021-05-05 22:52:58 +0200

    Login ablak
    A login ablak beállítása kezdőablaknak
    (Az előző commitok az intellij hibás commitjai)

--- src/main/java/magus/Main.java
+++ src/main/java/magus/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/views/mainWindow.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/views/loginWindow.fxml"));
         Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("/views/style.css").toExternalForm());
         primaryStage.setTitle("M.A.G.U.S");
